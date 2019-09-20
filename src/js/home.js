97b4d873855dc560885d6e9a52e745aee9a6b6a3
martinboykov <martinboykov@mail.bg>
2019-09-20 12:13:10 +0300

    mobile-navigation

--- src/js/home.js
+++ src/js/home.js
@@ -1,9 +1,10 @@
 
 document.addEventListener('DOMContentLoaded', function(event) {
   const mobileToggleBtn = document.body.querySelector('.toggle-button');
-
+  const nav = document.body.querySelector('.nav');
   mobileToggleBtn.addEventListener('click', function(ev) {
     mobileToggleBtn.classList.toggle('active');
+    nav.classList.toggle('active');
   });
 
   const navLinks = document.body.querySelectorAll('.menu__item');
